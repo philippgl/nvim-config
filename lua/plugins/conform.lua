181cb5a21da77b121d2395ad77e0d1009cde135c
Philipp Glaser <pglaser@cinemo.com>
2025-05-20 14:08:40 +0200

    format on save (+cmakeformat)

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,7 +1,45 @@
+vim.api.nvim_create_user_command("FormatDisable", function (args)
+    if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+    else
+        vim.g.disable_autoformat = true
+    end
+end, {
+    desc = "Disable autoformat-on-save",
+    bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function ()
+    vim.b.disable_autoformat = false
+    vim.g.disable_autoformat = false
+end, {
+    desc = "Re-enable autoformat-on-save",
+})
+
+local format_on_save = function (bufnr)
+    -- Disable with a global or buffer-local variable
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+    end
+    return { timeout_ms = 500, lsp_format = "fallback" }
+end
+
 return {
     "stevearc/conform.nvim",
     opts = {
+        formatters = {
+            cmakelangformat = {
+                command = "cmake-format",
+                -- if the formatter does not use stdin/out, it is assumed, that
+                -- it replaces the file in-place
+                args = { "-i", "$FILENAME" },
+                stdin = false,
+                tmpfile_format = ".conform.$RANDOM.$FILENAME",
+            },
+        },
         formatters_by_ft = {
+            cmake = { "cmakelangformat" },
+            cpp = { "clang_format" },
             lua = { "stylua" },
             -- Conform will run multiple formatters sequentially
             python = { "ruff_fix", "black" },
@@ -9,10 +47,7 @@ return {
             javascript = { "prettier" },
         },
 
-        format_on_save = {
-            -- These options will be passed to conform.format()
-            timeout_ms = 1000,
-            lsp_fallback = true,
-        },
+        format_on_save = format_on_save,
+        log_level = vim.log.levels.DEBUG,
     },
 }
